ba38d7078e7aead0eebfb75a0539a58092c0fa13
Karim Altir <mhdkarim48850@gmail.com>
2023-06-05 14:08:20 +0200

    created Persoon with Entity

---
+++ src/main/java/entiteit/Persoon.java
@@ -0,0 +1,53 @@
+package entiteit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Persoon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String email;
+
+    public Persoon() {
+    }
+
+    public Persoon(int userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
