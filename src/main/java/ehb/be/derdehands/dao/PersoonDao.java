e8ad30d34c86d7ddfa5d4ac86baf446d3b44ce52
Karim Altir <mhdkarim48850@gmail.com>
2023-06-06 19:16:39 +0200

    creating interface for persoon

---
+++ src/main/java/ehb/be/derdehands/dao/PersoonDao.java
@@ -0,0 +1,8 @@
+package ehb.be.derdehands.dao;
+
+import ehb.be.derdehands.entiteit.Persoon;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersoonDao extends CrudRepository<Persoon, Integer> {
+
+}
