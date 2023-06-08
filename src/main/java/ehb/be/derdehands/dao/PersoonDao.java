2a82f446fda1f4dab442f4b36e046a1c25ca8b43
Karim Altir <mhdkarim48850@gmail.com>
2023-06-08 16:10:53 +0200

    added query for email

--- src/main/java/ehb/be/derdehands/dao/PersoonDao.java
+++ src/main/java/ehb/be/derdehands/dao/PersoonDao.java
@@ -3,6 +3,9 @@ package ehb.be.derdehands.dao;
 import ehb.be.derdehands.entiteit.Persoon;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PersoonDao extends CrudRepository<Persoon, Integer> {
 
+    Optional<Persoon> findByEmail(String email);
 }
