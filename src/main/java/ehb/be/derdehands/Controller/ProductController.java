80405cb9def60661ed7484a28bb73e185cc8af20
Karim Altir <mhdkarim48850@gmail.com>
2023-06-09 10:34:21 +0200

    adding RedirectAttributes for success message

--- src/main/java/ehb/be/derdehands/Controller/ProductController.java
+++ src/main/java/ehb/be/derdehands/Controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -58,7 +59,8 @@ public class ProductController {
     public String insertNewProductAndNewPersoon(@Valid @ModelAttribute("savingProduct") Product product,
                                                 BindingResult productBindingResult,
                                                 @Valid @ModelAttribute("savingPersoon") Persoon persoon,
-                                                BindingResult persoonBindingResult) {
+                                                BindingResult persoonBindingResult,
+                                                RedirectAttributes redirectAttributes) {
         if (productBindingResult.hasErrors() || persoonBindingResult.hasErrors()) {
             return "/new";
         }
@@ -74,19 +76,28 @@ public class ProductController {
 
         mProductDao.save(product);
 
+        redirectAttributes.addFlashAttribute("successMessage", "Product saved successfully!");
+
         return "redirect:/index";
     }
 
+
     @GetMapping("/details/{id}")
-    public String showProductEnPersoonDetails(@PathVariable(value = "id") int id,
-                                     ModelMap modelMap) {
-        Optional<Persoon> persoonOptional = mPersoonDao.findById(id);
+    public String showProductEnPersoonDetails(@PathVariable(value = "id") int id, ModelMap modelMap) {
         Optional<Product> productOptional = mProductDao.findById(id);
 
-        if (persoonOptional.isPresent() && productOptional.isPresent()) {
-            modelMap.addAttribute("persoon", persoonOptional.get());
-            modelMap.addAttribute("product", productOptional.get());
-            return "details";
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            modelMap.addAttribute("product", product);
+
+            // Use the person ID associated with the product
+            int personId = product.getPersoon().getUserId();
+            Optional<Persoon> persoonOptional = mPersoonDao.findById(personId);
+
+            if (persoonOptional.isPresent()) {
+                modelMap.addAttribute("persoon", persoonOptional.get());
+                return "details";
+            }
         }
 
         return "redirect:/index";
